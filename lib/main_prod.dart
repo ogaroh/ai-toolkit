adfd5595a9ccbee66d126a56685afb292a0eb4f3
Erick Ogaro <ogaroerick1998@gmail.com>
2025-06-11 16:06:01 +0300

    Added Flavors to the project.

---
+++ lib/main_prod.dart
@@ -0,0 +1,11 @@
+import 'package:ai_toolkit/flavors.dart';
+import 'main.dart';
+
+// * Entry point for the prod flavor
+void main() async {
+  // Add environnement dependent code here
+  // ...
+  
+  F.appFlavor = Flavor.prod;
+  await runMainApp();
+}
